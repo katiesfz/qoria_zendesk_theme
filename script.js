00b93471005d11ed86ed55fd296f8b53a3287cd7
katiesfz <56288271+katiesfz@users.noreply.github.com>
2026-05-12 04:47:07 +0000

    Deploying to staging from @ katiesfz/qoria_zendesk_theme@eedc0256d5d7248106a289332e3454ae3a127c73 🚀

--- script.js
+++ script.js
@@ -1115,26 +1115,4 @@
     });
   });
 
-  document.querySelectorAll('pre:has(>code)').forEach((preElement) => {
-    const button = document.createElement('button');
-    button.innerHTML = '<i class="far fa-copy"></i>';
-    button.classList.add('code-copy-button');
-    button.dataset.tooltip = "Copy to clipboard";
-
-    preElement.appendChild(button);
-
-    button.addEventListener('click', () => {
-      // Specifically grab the code inside the <code> tag
-      const codeElement = preElement.querySelector('code');
-      const codeText = codeElement.innerText;
-
-      navigator.clipboard.writeText(codeText).then(() => {
-        button.classList.add('copied');
-        setTimeout(() => {
-          button.classList.remove('copied');
-        }, 2000);
-      });
-    });
-  });
-
 })();
