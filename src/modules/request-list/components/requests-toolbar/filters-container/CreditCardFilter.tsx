47ae1dbd75dd29f0fff8006351dc8dae0f306bd1
katiesfz <56288271+katiesfz@users.noreply.github.com>
2026-05-12 13:44:16 +0800

    fix: credit card filter field

--- src/modules/request-list/components/requests-toolbar/filters-container/CreditCardFilter.tsx
+++ src/modules/request-list/components/requests-toolbar/filters-container/CreditCardFilter.tsx
@@ -3,7 +3,7 @@ import type { ChangeEvent } from "react";
 import { useEffect, useState } from "react";
 import styled from "styled-components";
 import { FieldError } from "./FieldError";
-import type { FilterTypeKey, FilterTypeValue } from "./FilterTypeDropdown";
+// import type { FilterTypeKey, FilterTypeValue } from "./FilterTypeDropdown";
 import type { FilterValuesMap } from "../../../data-types/FilterValue";
 import type { FilterProperty } from "./FilterPropertyField";
 import { FilterTypeDropdown } from "./FilterTypeDropdown";
@@ -12,20 +12,18 @@ import { useTranslation } from "react-i18next";
 import {getFilterKey} from "./SystemFieldCheck";
 import { FilterValue } from "../../../data-types";
 
+const filterOptions = ["anyValue", "exactMatch"] as const;
+type FilterType = typeof filterOptions[number];
+
+type FilterTypeKey = "filterType";
 type FormFieldKey = FilterTypeKey | "cardNumber";
 
 const CARD_NUMBER_REGEX = /^\d{4}$/;
 
-const Container = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: ${(p) => p.theme.space.md};
-`;
-
 interface CreditCardFilterProps {
   filters: FilterValuesMap;
   filterProperty: FilterProperty;
-  onSelect: (state: FormState<FormFieldKey>) => void;
+  onSelect: (state: FormState) => void;
   errors: FormErrors<FormFieldKey>;
 }
 
@@ -42,23 +40,23 @@ export function CreditCardFilter({
   const validateForm = (
     cardNumber: string
   ): FormState<FormFieldKey> => {
-      if (cardNumber != "") {
-        if (CARD_NUMBER_REGEX.test(cardNumber)) {
-          return { state: "valid", values: [`:*${cardNumber}`] };
-        } else {
-          return {
-            state: "invalid",
-            errors: {
-              cardNumber: t(
-                "guide-requests-app.filter-modal.credit-card-digits-format-error",
-                "Enter the last four digits of the credit card, using only numbers"
-              ),
-            },
-          };
-        }
+    if (cardNumber != "") {
+      if (CARD_NUMBER_REGEX.test(cardNumber)) {
+        return { state: "valid", values: [`:*${cardNumber}`] };
       } else {
-        return { state: "valid", values: [":*"] };
+        return {
+          state: "invalid",
+          errors: {
+            cardNumber: t(
+              "guide-requests-app.filter-modal.credit-card-digits-format-error",
+              "Enter the last four digits of the credit card, using only numbers"
+            ),
+          },
+        };
       }
+    } else {
+      return { state: "valid", values: [":*"] };
+    }
   };
 
   //const handleFilterTypeSelect = (value: FilterTypeValue) => {
@@ -66,28 +64,28 @@ export function CreditCardFilter({
   //  onSelect(validateForm(cardNumber));
   //};
 
+  const filterKey = getFilterKey(filterProperty.identifier);
+
+  useEffect(() => {
+    const currentFilterValues = filters[filterKey] as FilterValue[] || [] as FilterValue[];
+    if (currentFilterValues.length > 0) {
+      const currentValue = currentFilterValues[0] as string;
+      if (currentValue === ":*") {
+        setCardNumber("");
+        return;
+      }
+      const rawValue = currentValue.replace(/^:\*(.*)$/, "$1");
+      setCardNumber(rawValue);
+    } else {
+        setCardNumber("");
+    }
+  }, [filters[filterKey]]);
+
   const handleCardNumberChanged = (e: ChangeEvent<HTMLInputElement>) => {
     const value = e.target.value;
     setCardNumber(value);
     onSelect(validateForm(value));
   };
-
-  const filterKey = getFilterKey(filterProperty.identifier);
-
-    useEffect(() => {
-      const currentFilterValues = filters[filterKey] as FilterValue[] || [] as FilterValue[];
-      if (currentFilterValues.length > 0) {
-        const currentValue = currentFilterValues[0] as string;
-        if (currentValue === ":*") {
-          setCardNumber("");
-          return;
-        }
-        const rawValue = currentValue.replace(/^:"(.*)"$/, "$1");
-        setCardNumber(rawValue);
-      } else {
-          setCardNumber("");
-      }
-    }, [filters[filterKey]]);
 
   return (
     <>
